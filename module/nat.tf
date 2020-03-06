b4db3f1d1b4964624b958917a0764e9d4ddc4a5b
Aruna Lakmal <arunarandunu@gmail.com>
2020-03-06 22:57:23 +0530

    space removed

--- module/nat.tf
+++ module/nat.tf
@@ -1,7 +1,7 @@
 resource "aws_nat_gateway" "tc_nat_gw" {
   allocation_id = "${aws_eip.tc_eip.id}"
   subnet_id     = "${aws_subnet.tc_pub2_sub.id}"
-  depends_on    = ["${aws_internet_gateway.tc_igw}"]
+  depends_on    = ["aws_internet_gateway.tc_igw"]
 
   tags = {
     Name = "tc_ngw"
