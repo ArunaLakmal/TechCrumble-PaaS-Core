74338bf2e5510567aef55879c601a29ed7f54752
Aruna Lakmal <arunarandunu@gmail.com>
2020-03-07 09:26:05 +0530

    Update variables.tf

--- module/variables.tf
+++ module/variables.tf
@@ -1,12 +1,8 @@
 variable "aws_region" {}
-<<<<<<< HEAD
-
 variable "aws_profile" {
   default = "default"
 }
-=======
 variable "aws_profile" {}
->>>>>>> 723fa0cb0442a3c2b1e243e93d5d35c13063eb75
 data "aws_availability_zones" "available" {}
 variable "vpc_cidr" {}
 
